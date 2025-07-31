6f2174d880a5cbef3cbd653bdfdfa2fbdbf9b949
Siggy Nazimowitz <thewotter@icloud.com>
2025-07-31 12:10:18 -0400

    upload from neocities to new repository

---
+++ email.php
@@ -0,0 +1,5 @@
+<?php
+    mail('thewotter@icloud.com', $_POST['contact'], $_POST['contact-info'], $_POST['comm-type'], $_POST['details'], $_POST['references']);
+?>
+<p>Thank you so much for the opportunity! I will respond to your inquiry ASAP!</p>
+
